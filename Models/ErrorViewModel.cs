3cc46401f6d56c136b4d23f3baf2e99e67aac2a2
SUTEE FUFUANG <6704917@rsu.ac.th>
2026-01-16 20:07:15 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RSU_360_X.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
